740af9d469dc46c9244cc54e9fd6fc1110d0234d
gangelion <gangelion8@gmail.com>
2020-06-19 11:38:53 +0900

    404エラー時の画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get '/users/sign_in' => 'devise/sessions#new', as: :new_user_session
+    post '/users/sign_in' => 'devise/sessions#create', as: :user_session
+    delete '/users/sign_out' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root to: "articles#index"
 
   resources :articles
@@ -9,4 +15,5 @@ Rails.application.routes.draw do
 
   resources :inquiries, only: %i[index new create show]
 
+  get '*path', controller: 'application', action: 'render_404'
 end
